a8fc99842237389ff7d91874af8a62f813281b8c
Sam Cutter <swcutter@pm.me>
2024-09-15 18:00:42 +0100

    Exported to lib crate, created basic user interface

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
-mod evaluate;
-mod token;
+use inquire::Text;
+use postfix::{evaluate, token};
 
-fn main() {}
+fn main() {
+    loop {
+        let expression = match Text::new("Enter expression to be evaluated (enter to exit): ").prompt() {
+            Ok(expression) => {
+                if expression == "" { break }
+                
+                expression
+            },
+            Err(_) => {
+                println!("Error!");
+                continue;
+            }
+        };
+
+        let token_sequence = match token::tokenize(&expression) {
+            Ok(token_sequence) => token_sequence,
+            Err(error) => {
+                println!("{}", error);
+                continue;
+            }
+        };
+
+        let evaluation = match evaluate::evaluate(token_sequence) {
+            Ok(evaluation) => evaluation,
+            Err(error) => {
+                println!("{}", error);
+                continue;
+            }
+        };
+
+        println!("{} = {}", expression, evaluation);
+    }
+}
