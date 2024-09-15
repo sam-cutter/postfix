292d41e1625fc7ae3ad138a077bce7cfdad3cc01
Sam Cutter <swcutter@pm.me>
2024-09-15 11:35:41 +0100

    Made janky, but functioning evaluation function

--- src/main.rs
+++ src/main.rs
@@ -36,15 +36,35 @@ fn tokenize(input: String) -> Result<Vec<Token>, String> {
     return Ok(tokens);
 }
 
-fn evaluate(expression: Vec<Token>) {
+fn evaluate(expression: Vec<Token>) -> f64 {
+    let mut stack: Vec<f64> = Vec::new();
+
+    for token in expression {
+        match token {
+            Token::Operand(number) => stack.push(number),
+            Token::Operator(operator) => {
+                let second = stack.pop().unwrap();
+                let first = stack.pop().unwrap();
+
+                match operator {
+                    Operator::Add => stack.push(first + second),
+                    Operator::Subtract => stack.push(first - second),
+                    Operator::Multiply => stack.push(first * second),
+                    Operator::Divide => stack.push(first / second),
+                }
+            }
+        }
+    }
+
+    return stack.pop().unwrap();
 }
     
     
 fn main() {
-    let tokens = tokenize(String::from("1 2 +"));
+    let tokens = tokenize(String::from("9 3 /"));
 
     match tokens {
-        Ok(tokens) => evaluate(tokens),
+        Ok(tokens) => println!("{}", evaluate(tokens)),
         Err(e) => println!("{e}"),
     }
 }
