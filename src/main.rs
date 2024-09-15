d8553702ccf38205b9d1e1acf1771bc410d42f2f
Sam Cutter <swcutter@pm.me>
2024-09-15 11:26:31 +0100

    Made tokenize function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+enum Token {
+    Operand(f64),
+    Operator(Operator)
+}
+
+enum Operator {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
+
+fn tokenize(input: String) -> Result<Vec<Token>, String> {
+
+    let mut tokens: Vec<Token> = Vec::new();
+
+    for word in input.split(' ') {
+        let token: Result<Token, String> = match word {
+            "+" => Ok(Token::Operator(Operator::Add)),
+            "-" => Ok(Token::Operator(Operator::Subtract)),
+            "*" => Ok(Token::Operator(Operator::Multiply)),
+            "/" => Ok(Token::Operator(Operator::Divide)),
+            _ =>  match word.parse::<f64>() {
+                Ok(number) => Ok(Token::Operand(number)),
+                Err(_) => Err(format!("Error parsing token: {word} is not a valid token."))
+            }
+        };
+
+        match token {
+            Ok(token) => tokens.push(token),
+            Err(e) => return Err(e)
+        }
+
+    }
+
+    return Ok(tokens);
+}
+
+fn evaluate(expression: Vec<Token>) {
+}
+    
+    
 fn main() {
-    println!("Hello, world!");
+    let tokens = tokenize(String::from("1 2 +"));
+
+    match tokens {
+        Ok(tokens) => evaluate(tokens),
+        Err(e) => println!("{e}"),
+    }
 }
